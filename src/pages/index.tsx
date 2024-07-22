9af2d100cacc49862bc468be2ed4be8b8815390d
Lilysaria <bloomyday56@gmail.com>
2024-07-22 13:42:24 -0700

    style(ui): modify style elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ import GlowEffect from '../components/GlowEffect/GlowEffect';
 import Stars from '../components/Stars/Stars';
 import GreatRift from '../components/GreatRift/GreatRift';
 import NavBar from '../components/NavBar/NavBar';
-import ExpandableCard, { Project } from '../components/ExpandableCard/ExpandableCard';
+import ExpandableCard, {
+  Project,
+} from '../components/ExpandableCard/ExpandableCard';
 import NatureParallax from '../components/NatureParallax/NatureParallax';
 import Modal from '../components/Modal/Modal';
 import CardButton from '../components/CardButton/CardButton';
@@ -25,14 +27,14 @@ function Index(): React.ReactElement {
 
   useEffect(() => {
     fetch('/api/projects')
-      .then((response) => response.json())
-      .then((data) => setProjects(data));
+      .then(response => response.json())
+      .then(data => setProjects(data));
   }, []);
 
   const handleProjectCreated = () => {
     fetch('/api/projects')
-      .then((response) => response.json())
-      .then((data) => setProjects(data));
+      .then(response => response.json())
+      .then(data => setProjects(data));
   };
 
   return (
@@ -42,17 +44,35 @@ function Index(): React.ReactElement {
       <GlowEffect />
       <Stars />
       <CardButton onProjectCreated={handleProjectCreated} />
-      <NavBar setShowSection={setShowSection} setParallaxVisible={setParallaxVisible} />
+      <NavBar
+        setShowSection={setShowSection}
+        setParallaxVisible={setParallaxVisible}
+      />
       <div className="absolute top-0 left-0 w-full flex justify-between items-center p-5">
         <div className="flex flex-col items-start">
           <h1 className="text-xl font-bold mb-12 text-white">
             {/* eslint-disable-next-line */}
             Lilysaria Gaska
           </h1>
-          <button type="button" className="text-lg mt-2 mb-12 hover:underline cursor-pointer text-white bg-transparent border-none" onClick={() => setModalOpen('Contact')}>Contact +</button>
-          <button type="button" className="text-lg hover:underline cursor-pointer text-white bg-transparent border-none" onClick={() => setModalOpen('About')}>About +</button>
+          <button
+            type="button"
+            className="text-lg mt-2 mb-12 hover:underline cursor-pointer text-white bg-transparent border-none"
+            onClick={() => setModalOpen('Contact')}
+          >
+            Contact +
+          </button>
+          <button
+            type="button"
+            className="text-lg hover:underline cursor-pointer text-white bg-transparent border-none"
+            onClick={() => setModalOpen('About')}
+          >
+            About +
+          </button>
         </div>
-        <div className="absolute left-1/2 top-0 text-center" style={{ transform: 'translateX(-60%)' }}>
+        <div
+          className="absolute left-1/2 top-0 text-center"
+          style={{ transform: 'translateX(-60%)' }}
+        >
           <h2 className="text-lg text-white">Web Developer</h2>
           <p className="text-sm mt-1 text-white">Murphy, Oregon</p>
         </div>
@@ -70,12 +90,11 @@ function Index(): React.ReactElement {
               gap: '1rem',
             }}
           >
-            {projects.filter((project) => project.section === 'work').map((project) => (
-              <ExpandableCard
-                key={project.id}
-                project={project}
-              />
-            ))}
+            {projects
+              .filter(project => project.section === 'work')
+              .map(project => (
+                <ExpandableCard key={project.id} project={project} />
+              ))}
           </div>
         )}
         {showSection === 'playground' && (
@@ -90,12 +109,11 @@ function Index(): React.ReactElement {
               gap: '1rem',
             }}
           >
-            {projects.filter((project) => project.section === 'playground').map((project) => (
-              <ExpandableCard
-                key={project.id}
-                project={project}
-              />
-            ))}
+            {projects
+              .filter(project => project.section === 'playground')
+              .map(project => (
+                <ExpandableCard key={project.id} project={project} />
+              ))}
           </div>
         )}
         {showSection === 'writings' && (
@@ -110,12 +128,11 @@ function Index(): React.ReactElement {
               gap: '1rem',
             }}
           >
-            {projects.filter((project) => project.section === 'writings').map((project) => (
-              <ExpandableCard
-                key={project.id}
-                project={project}
-              />
-            ))}
+            {projects
+              .filter(project => project.section === 'writings')
+              .map(project => (
+                <ExpandableCard key={project.id} project={project} />
+              ))}
           </div>
         )}
       </div>
@@ -127,14 +144,21 @@ function Index(): React.ReactElement {
         title="About"
       >
         <div className="about-container">
-          <div className="hello-header">
+          <div className="hello-header mb-2">
             <h2 className="hello-text">Hello,</h2>
           </div>
-          <div className="paragraph-content">
+          <div className="paragraph-content mt-[-30px]">
             <p>
-              I am a front-end developer with a passion for creating visually
-              appealing and user-friendly websites. I value collaboration and am
-              always eager to adopt new skills and technologies.
+              My name is Lily, an aspiring web developer living in Oregon.
+              Inspired by other developers and the changing web sphere, I hope
+              to develop alongside others. I have a passion for front-end and
+              back-end technologies and web design. I have discovered my
+              favorite approaches are more methodical, with an openness to being
+              creative. I am deeply committed to learning the web and am excited
+              to continue expanding my knowledge with new technologies like
+              state management libraries (such as Redux), TypeScript,
+              server-side rendering with Next.js, and Python, as well as
+              emerging areas like cloud computing and DevOps.
             </p>
           </div>
         </div>
@@ -147,11 +171,45 @@ function Index(): React.ReactElement {
       >
         <div className="contact-container text-center">
           <div className="flex flex-col items-center mt-4">
-            <button className="btn-6 mb-2" onClick={() => window.open('mailto:lilysaria@protonmail.com', '_blank')}><span>lilysaria@protonmail.com</span></button>
-            <button className="btn-6 mb-2" onClick={() => window.open('https://github.com/Lilysaria', '_blank')}><span>GitHub</span></button>
-            <button className="btn-6 mb-2" onClick={() => window.open('https://www.linkedin.com/in/lilysaria-gaska/', '_blank')}><span>LinkedIn</span></button>
-            <button className="btn-6 mb-2" onClick={() => window.open('path_to_your_resume.pdf', '_blank')}><span>Resume</span></button>
-            <button className="btn-6 mb-2" onClick={() => window.location.href = 'tel:+15417616895'}><span>541-761-6895</span></button>
+            <button
+              className="btn-6 mb-2"
+              onClick={() =>
+                window.open('mailto:lilysaria@protonmail.com', '_blank')
+              }
+            >
+              <span>lilysaria@protonmail.com</span>
+            </button>
+            <button
+              className="btn-6 mb-2"
+              onClick={() =>
+                window.open('https://github.com/Lilysaria', '_blank')
+              }
+            >
+              <span>GitHub</span>
+            </button>
+            <button
+              className="btn-6 mb-2"
+              onClick={() =>
+                window.open(
+                  'https://www.linkedin.com/in/lilysaria-gaska/',
+                  '_blank',
+                )
+              }
+            >
+              <span>LinkedIn</span>
+            </button>
+            <button
+              className="btn-6 mb-2"
+              onClick={() => window.open('path_to_your_resume.pdf', '_blank')}
+            >
+              <span>Resume</span>
+            </button>
+            <button
+              className="btn-6 mb-2"
+              onClick={() => (window.location.href = 'tel:+15417616895')}
+            >
+              <span>541-761-6895</span>
+            </button>
           </div>
         </div>
       </Modal>
